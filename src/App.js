ab01d56801b2d96b06c9959e7b2c6f5e35060aa1
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-14 14:13:54 +0100

    Change App

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. I did all that. I saved.
           </p>
           <a
             className="App-link"
