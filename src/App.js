3a9ed2159f4990909137865ad1071c24dd7fa4d3
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-14 14:21:35 +0100

    Add fetch to remote server

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '',
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://portfolio-diana-eu-1.herokuapp.com/');
+    const json = await response.json();
+    this.setState({ data: json.data });
+  }
   render() {
+    const data = this.state.data;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. I did all that. I saved.
-          </p>
+          {data ? <p>The data is {data}</p> : <p>No data yet</p>}
           <a
             className="App-link"
             href="https://reactjs.org"
